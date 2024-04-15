146039123ab63fe2a856a8ed5af5396781070f87
mikoo17 <m.drojma@gmail.com>
2024-04-15 22:06:14 +0200

    V2.2 fully working service, with basic test Integration and controller unit test

--- src/main/java/com/project/rest/controller/ProjektRestController.java
+++ src/main/java/com/project/rest/controller/ProjektRestController.java
@@ -18,7 +18,7 @@ import java.net.URI;
 @RestController
 @RequestMapping("/api")
 public class ProjektRestController {
-    private ProjektService projektService;
+    private static ProjektService projektService = null; // dodane, ponieważ metoda z pageable nie działała prawidłowo
 
     @Autowired
     public ProjektRestController(ProjektService projektService) {
@@ -27,13 +27,13 @@ public class ProjektRestController {
     // PRZED KAŻDĄ Z PONIŻSZYCH METOD JEST UMIESZCZONA ADNOTACJA (@GetMapping, PostMapping, ... ), KTÓRA OKREŚLA
     // RODZAJ METODY HTTP, A TAKŻE ADRES I PARAMETRY ŻĄDANIA
     @GetMapping("/projekty/{projektId}")
-    ResponseEntity<Projekt> getProjekt(@PathVariable Integer projektId) {// @PathVariable oznacza, że wartość
+    public ResponseEntity<Projekt> getProjekt(@PathVariable Integer projektId) {// @PathVariable oznacza, że wartość
         return ResponseEntity.of(projektService.getProjekt(projektId)); // parametru przekazywana jest w ścieżce
     }
     // @Valid włącza automatyczną walidację na podstawie adnotacji zawartych
     // w modelu np. NotNull, Size, NotEmpty itd. (z jakarta.validation.constraints.*)
     @PostMapping(path = "/projekty")
-    ResponseEntity<Void> createProjekt(@Valid @RequestBody Projekt projekt) {// @RequestBody oznacza, że dane
+    public ResponseEntity<Void> createProjekt(@Valid @RequestBody Projekt projekt) {// @RequestBody oznacza, że dane
         // projektu (w formacie JSON) są
         Projekt createdProjekt = projektService.setProjekt(projekt); // przekazywane w ciele żądania
         URI location = ServletUriComponentsBuilder.fromCurrentRequest() // link wskazujący utworzony projekt
@@ -59,14 +59,14 @@ public class ProjektRestController {
     }
     //Przykład żądania wywołującego metodę: http://localhost:8080/api/projekty?page=0&size=10&sort=nazwa,desc
     @GetMapping(value = "/projekty")
-    Page<Projekt> getProjekty(Pageable pageable) { // @RequestHeader HttpHeaders headers – jeżeli potrzebny
+    public static Page<Projekt> getProjekty(Pageable pageable) { // @RequestHeader HttpHeaders headers – jeżeli potrzebny
         return projektService.getProjekty(pageable); // byłby nagłówek, wystarczy dodać drugą zmienną z adnotacją
     }
 
     // Przykład żądania wywołującego metodę: GET http://localhost:8080/api/projekty?nazwa=webowa
     // Metoda zostanie wywołana tylko, gdy w żądaniu będzie przesyłana wartość parametru nazwa.
     @GetMapping(value = "/projekty", params="nazwa")
-    Page<Projekt> getProjektyByNazwa(@RequestParam String nazwa, Pageable pageable) {
+    public Page<Projekt> getProjektyByNazwa(@RequestParam String nazwa, Pageable pageable) {
         return projektService.searchByNazwa(nazwa, pageable);
     }
 }
